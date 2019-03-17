c0b14e2357404f5c9536e27fff7395e6ca6bfe80
Parth Mehrotra <parth@mehrotra.me>
2019-03-17 12:30:48 -0400

    docs folder for gh-pages

---
+++ docs/precache-manifest.561b0209713d81700da918a02a8c932b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0b97b49f66d815b4e0c",
+    "url": "/static/css/main.9c05a030.chunk.css"
+  },
+  {
+    "revision": "f0b97b49f66d815b4e0c",
+    "url": "/static/js/main.3a02809a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c83175b9a4c58bef9ff6",
+    "url": "/static/js/2.73df7435.chunk.js"
+  },
+  {
+    "revision": "d299e955d3b4458d785ab668e9fa7ebf",
+    "url": "/index.html"
+  }
+];

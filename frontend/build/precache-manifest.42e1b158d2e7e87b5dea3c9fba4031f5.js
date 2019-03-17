2b24ebf1c63357ff2f46e89fd92e2e5c14706b37
Parth Mehrotra <parth@mehrotra.me>
2019-03-17 12:26:19 -0400

    prod build

---
+++ frontend/build/precache-manifest.42e1b158d2e7e87b5dea3c9fba4031f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b16b5b39da76caee4d6e",
+    "url": "/static/css/main.9c05a030.chunk.css"
+  },
+  {
+    "revision": "b16b5b39da76caee4d6e",
+    "url": "/static/js/main.b8062bce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c83175b9a4c58bef9ff6",
+    "url": "/static/js/2.73df7435.chunk.js"
+  },
+  {
+    "revision": "41f90d8ae52d00a58d59a06ca9f62e8b",
+    "url": "/index.html"
+  }
+];
